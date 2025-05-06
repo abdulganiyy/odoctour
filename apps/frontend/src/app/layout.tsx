8fe8429695c7430a6a9d819a53b166318768edf2
abdul <abdul@servant.io>
2025-05-06 17:51:54 +0100

    adding password reset features

--- apps/frontend/src/app/layout.tsx
+++ apps/frontend/src/app/layout.tsx
@@ -1,8 +1,8 @@
-"use client";
 import { Toaster } from "@/components/ui/toaster";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Suspense } from "react";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,8 +29,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
-        <Toaster />
+        <Suspense>
+          {children}
+          <Toaster />
+        </Suspense>
       </body>
     </html>
   );
